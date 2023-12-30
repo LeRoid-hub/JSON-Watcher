747fa91f66e84b95a0e6ac9bb45390ae0e859552
Jan Barfuß <janbar2001@gmail.com>
2023-12-30 22:46:29 +0100

    Initial Commit

---
+++ Json-Watcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Json-Watcher
+//
+//  Created by Jan Barfuß on 30.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Json-Watcher/Json_WatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Json_WatcherApp.swift
+//  Json-Watcher
+//
+//  Created by Jan Barfuß on 30.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Json_WatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
